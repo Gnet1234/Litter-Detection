4ac1b76b1731651bfff2cc6f1fd2c2e567cc3bdf
Gnet1234 <garnetmax02@gmail.com>
2024-05-23 12:06:07 -0400

    Add files via upload

---
+++ Connection_Test.py
@@ -0,0 +1,25 @@
+import olympe
+
+# To use this code paste, python /mnt/c/Drone/venv/Connection_Test.py.
+# IP address of the drone
+DRONE_IP = "192.168.42.21"  # Replace with your drone's IP address
+
+def test_connection():
+    # Create the olympe.Drone object from its IP address
+    drone = olympe.Drone(DRONE_IP)
+
+    try:
+        # Connect to the drone
+        drone.connect()
+
+        # If the connection is successful, print a success message
+        #print("Drone connection successful!")
+
+        # Disconnect from the drone
+        drone.disconnect()
+    except Exception as e:
+        # If an error occurs during connection, print the error message
+        print(f"Error connecting to the drone: {e}")
+
+if __name__ == "__main__":
+    test_connection()
