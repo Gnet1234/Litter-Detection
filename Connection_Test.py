6935b2936e4797e72fd045cb073c29d51e920299
Gnet1234 <garnetmax02@gmail.com>
2024-06-15 13:29:57 -0400

    Update Connection_Test.py

--- Connection_Test.py
+++ Connection_Test.py
@@ -1,28 +1,22 @@
 import olympe
+import os
 import logging
+
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
 
-# IP address of the drone
-DRONE_IP = "192.168.42.21"  # Replace with your drone's IP address
+# Used to activate virtual environment: source /mnt/c/Drone/venv/bin/activate
+# Used to Start code: python3 /mnt/c/Drone/venv/Connect.py 
 
-def test_connection():
-    # Create the olympe.Drone object from its IP address
+# It can connect, but there is a syncronization issue causing it to disconnect. 
+
+def test_physical_drone():
     drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    drone.disconnect()
 
-    try:
-        # Connect to the drone
-        drone.connect()
-
-        # If the connection is successful, print a success message
-        print("Drone connection successful!")
-
-        # Disconnect from the drone
-        drone.disconnect()
-    except Exception as e:
-        # If an error occurs during connection, print the error message
-        print(f"Error connecting to the drone: {e}")
 
 if __name__ == "__main__":
-    test_connection()
+    test_physical_drone()
