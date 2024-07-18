f8ec6c0cf436d5f53aca0446592213a34c99d44f
Min Tun <mkt@my.yorku.ca>
2024-07-18 12:15:54 -0400

    Update Experiment_With_Video_Logic.py

--- Experiment_With_Video_Logic.py
+++ Experiment_With_Video_Logic.py
@@ -11,12 +11,13 @@ import multiprocessing
 import inputs  # Importing the gamepad library
 import olympe
 from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy
-from olympe.messages.ardrone3.PilotingState import FlyingStateChanged, GpsLocationChanged
-from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged
+from olympe.messages.ardrone3.PilotingState import GpsLocationChanged, FlyingStateChanged
+from olympe.messages.ardrone3.GPSSettingsState import GPSFixStateChanged, HomeChanged
 from ultralytics import YOLO
 import time
 import pandas as pd
 from datetime import datetime
+import curses  # Importing the curses library
 
 olympe.log.update_config({"loggers": {"olympe": {"level": "WARNING"}}})
 
@@ -31,23 +32,10 @@ model = YOLO(Model_Path)
 # Define debounce delay in seconds (adjust as needed)
 DEBOUNCE_DELAY = 0.3
 
-# Declare gps_data_df as global
-global gps_data_df
 # Define a global DataFrame to store GPS data
 gps_data_df = pd.DataFrame(columns=['Timestamp', 'Latitude', 'Longitude', 'Altitude'])
 
-def wait_for_gps_fix(drone):
-    """
-    Waits until the drone acquires a GPS fix.
-    """
-    gps_fix_acquired = False
-    while not gps_fix_acquired:
-        gps_fix_state = drone.get_state(GPSFixStateChanged)
-        gps_fix_acquired = gps_fix_state['fixed']
-        if not gps_fix_acquired:
-            print("Waiting for GPS fix...")
-            time.sleep(1)
-    print("GPS fix acquired.")
+# gps_data_df = pd.DataFrame(columns=['Timestamp', 'Latitude', 'Longitude', 'Altitude', 'x'])
 
 class StreamingExample:
     def __init__(self):
@@ -57,6 +45,7 @@ class StreamingExample:
         self.tempd = tempfile.mkdtemp(prefix="olympe_streaming_test_")
         # Prints the path for the temporary directory.
         print(f"Olympe streaming example output dir: {self.tempd}")
+        """""""""
         # Opens an empty list to store h264 statistics.
         self.h264_frame_stats = []
         # Opens a temporary directory for writing video stream statistics.
@@ -70,14 +59,14 @@ class StreamingExample:
         self.processes = []
         self.running = multiprocessing.Event()
         self.running.set()
-
+        """""
     def start(self):
         # Connect to drone
         assert self.drone.connect(retry=3)
-
+        """""""""
         if DRONE_RTSP_PORT is not None:
             self.drone.streaming.server_addr = f"{DRONE_IP}:{DRONE_RTSP_PORT}"
-
+        
         # You can record the video stream from the drone if you plan to do some
         # post processing.
         self.drone.streaming.set_output_files(
@@ -106,8 +95,9 @@ class StreamingExample:
         # Start the video output processing thread
         self.output_thread = threading.Thread(target=self.process_video_output)
         self.output_thread.start()
-
+        """""
     def stop(self):
+        """""""""
         self.running.clear()
         for p in self.processes:
             p.join()
@@ -118,13 +108,10 @@ class StreamingExample:
         assert self.drone.streaming.stop()
         assert self.drone.disconnect()
         self.h264_stats_file.close()
-
+        """""
+    """""""""
     def yuv_frame_cb(self, yuv_frame):
-        """
-        This function will be called by Olympe for each decoded YUV frame.
-
-            :type yuv_frame: olympe.VideoFrame
-        """
+       
         yuv_frame.ref()
         info = yuv_frame.info()
         height, width = info["raw"]["frame"]["info"]["height"], info["raw"]["frame"]["info"]["width"]
@@ -133,7 +120,6 @@ class StreamingExample:
         yuv_frame.unref()
 
     def yuv_frame_processing(self, running):
-        global gps_data_df
         while running.is_set():
             try:
                 yuv_data, height, width = self.frame_queue.get(timeout=0.1)
@@ -146,38 +132,6 @@ class StreamingExample:
             # Plot boundary & image masking from obtained results in f2f format
             annotated_frame = results[0].plot()
             self.processed_frame_queue.put(annotated_frame)
-            boxes = None
-
-            for result in results:
-                boxes = result.boxes.xyxy
-
-            if boxes is not None and boxes.size(0) > 0:
-                print("Objects detected!")
-                # Wait for GPS location change
-                gps_data = None
-                while gps_data is None:
-                    try:
-                        gps_data = self.drone.get_state(GpsLocationChanged)
-                    except ValueError:
-                        print("Waiting for GPS data...")
-                        time.sleep(1)
-                
-                # Extract coordinates, and records time when it happens.
-                timestamp = datetime.now()
-                latitude = gps_data['latitude']
-                longitude = gps_data['longitude']
-                altitude = gps_data['altitude']
-
-                # Print the GPS coordinates
-                print("Latitude: {:.7f}, Longitude: {:.7f}, Altitude: {:.2f}".format(latitude, longitude, altitude))
-
-                # Adds the recorded data to the data frame (gps_data_df).
-                gps_data_df = pd.concat([gps_data_df, pd.DataFrame({
-                    'Timestamp': [timestamp],
-                    'Latitude': [latitude],
-                    'Longitude': [longitude],
-                    'Altitude': [altitude]
-                })], ignore_index=True)
 
     def process_video_output(self):
         while self.running.is_set():
@@ -207,11 +161,7 @@ class StreamingExample:
         pass
 
     def h264_frame_cb(self, h264_frame):
-        """
-        This function will be called by Olympe for each new h264 frame.
-
-            :type yuv_frame: olympe.VideoFrame
-        """
+     
 
         # Get a ctypes pointer and size for this h264 frame
         frame_pointer, frame_size = h264_frame.as_ctypes_pointer()
@@ -234,10 +184,10 @@ class StreamingExample:
             h264_fps = len(self.h264_frame_stats)
             h264_bitrate = 8 * sum(map(lambda t: t[1], self.h264_frame_stats))
             self.h264_stats_writer.writerow({"fps": h264_fps, "bitrate": h264_bitrate})
+        """""
 
     def fly(self):
-        # Wait for GPS fix before flying
-        wait_for_gps_fix(self.drone)
+        global gps_data_df  # Declare gps_data_df as global
 
         # Initialize debounce timers for each button
         debounce_tl = time.time()
@@ -247,21 +197,82 @@ class StreamingExample:
         debounce_au = time.time()
         debounce_ad = time.time()
 
+        # Checks if motion occurred, and records the location data if it did.
+        Motion_Check = False
+
         try:
-            while self.running.is_set():
+            # Prints the location of the drone before taking off.
+            # Send the SetHome command
+
+            print("GPS position before take-off :", self.drone.get_state(HomeChanged))
+            
+            def handle_key_press(stdscr):
+                global Motion_Check
+                curses.cbreak()
+                stdscr.keypad(True)
+                stdscr.nodelay(True)  # Make getch() non-blocking
+                stdscr.addstr(0, 0, "Press 'r' to record GPS data. Press 'q' to quit.")
+
+                while True:
+                    key = stdscr.getch()
+                    if key == ord('r'):  # Replace 'r' with the key you want to check
+                        Motion_Check = True
+                    elif key == ord('q'):  # Press 'q' to exit the loop
+                        break
+
+            threading.Thread(target=curses.wrapper, args=(handle_key_press,)).start()
+
+            while True:
+                if Motion_Check:
+                    # Wait for GPS location change
+                    gps_data = self.drone.get_state(GpsLocationChanged)
+
+                    # Extract coordinates, and records time when it happens.
+                    timestamp = datetime.now()
+                    latitude = gps_data['latitude']
+                    longitude = gps_data['longitude']
+                    altitude = gps_data['altitude']
+
+                    # Print the GPS coordinates
+                    print("Latitude: {:.7f}, Longitude: {:.7f}, Altitude: {:.2f}".format(latitude, longitude, altitude))
+
+                    # Adds the recorded data to the data frame (gps_data_df).
+                    gps_data_df = pd.concat([gps_data_df, pd.DataFrame({
+                        'Timestamp': [timestamp],
+                        'Latitude': [latitude],
+                        'Longitude': [longitude],
+                        'Altitude': [altitude]
+                    })], ignore_index=True)
+
+                    # Sets the Boolean variable back to false.
+                    Motion_Check = False
+
+                # Creates an event based on the input of the gamepad.
                 events = inputs.get_gamepad()
+
+                # For loop that monitors the game-pads input, and produces an output that moves the drone.
                 for event in events:
+                    # There are two different event types for the gamepad, absolute which have a range of -35000 to 35000, and key which have are either 1 or 0.
+                    # The key events are the buttons, while absolute events are the sticks and triggers.
                     if event.ev_type == 'Absolute':
+                        # event.code is used to recognize the specific buttons.
                         if event.code == 'ABS_RY':
+                            # This variable stores the value produced from pressing the button.
                             y_value2 = event.state
+                            # Debounce time is used to limit the amount commands that are registered, since the buttons can be sensetive.
                             if y_value2 > 30000 and (time.time() - debounce_ad) > DEBOUNCE_DELAY:  # Downward
+                                # This moves the drone.
                                 self.drone(moveBy(0, 0, -1, 0)).wait()
                                 print("Moving Downwards")
+                                # This resets the debounce_ad, so the button can be registered again.
                                 debounce_ad = time.time()
+                                # Gives the command to print, and store the location data.
+                                Motion_Check = True
                             elif y_value2 < -30000 and (time.time() - debounce_au) > DEBOUNCE_DELAY:  # Upwards
                                 self.drone(moveBy(0, 0, 1, 0)).wait()
                                 print("Moving Upwards")
                                 debounce_au = time.time()
+                                Motion_Check = True
                             elif abs(y_value2) <= 30000:  # Deadzone for y_value2
                                 self.drone(moveBy(0, 0, 0, 0)).wait()  # Stop movement
                         elif event.code == 'ABS_Z':
@@ -269,53 +280,70 @@ class StreamingExample:
                                 self.drone(moveBy(0, -1, 0, 0)).wait()
                                 print("Moving Left")
                                 debounce_tul = time.time()
+                                Motion_Check = True
                         elif event.code == 'ABS_RZ':
                             if event.state <= 1000 and (time.time() - debounce_tur) > DEBOUNCE_DELAY:
                                 self.drone(moveBy(0, 1, 0, 0)).wait()
                                 print("Moving Right")
                                 debounce_tur = time.time()
+                                Motion_Check = True
                     elif event.ev_type == 'Key':
                         if event.code == 'BTN_SOUTH' and event.state == 1:  # A button
                             self.drone(TakeOff())
                             print("TakeOff")
+                            Motion_Check = True
                         elif event.code == 'BTN_EAST' and event.state == 1:  # B button
                             self.drone(Landing())
                             print("Landing")
+                            Motion_Check = True
                         elif event.code == 'BTN_WEST' and event.state == 1:  # Y button
                             self.drone(moveBy(0, 0, 0, 1)).wait()
                             print("Turning clockwise")
+                            Motion_Check = True
                         elif event.code == 'BTN_NORTH' and event.state == 1:  # X button
                             self.drone(moveBy(0, 0, 0, -1)).wait()
                             print("Turning counter clockwise")
+                            Motion_Check = True
                         elif event.code == 'BTN_TL':
                             if event.state == 1 and (time.time() - debounce_tl) > DEBOUNCE_DELAY:
                                 self.drone(moveBy(-1, 0, 0, 0)).wait()
                                 print("Moving Backwards")
                                 debounce_tl = time.time()
+                                Motion_Check = True
                         elif event.code == 'BTN_TR':
                             if event.state == 1 and (time.time() - debounce_tr) > DEBOUNCE_DELAY:
                                 self.drone(moveBy(1, 0, 0, 0)).wait()
                                 print("Moving Forwards")
                                 debounce_tr = time.time()
+                                Motion_Check = True
 
                         # Add more mappings for other buttons as needed
         except KeyboardInterrupt:
             print("Stopping control with Xbox controller...")
 
+            print("GPS monitoring stopped.")
+            # Save data to Excel file
+            save_to_excel(gps_data_df)
+
         print("Landing...")
         self.drone(Landing() >> FlyingStateChanged(state="landed", _timeout=5)).wait()
         print("Landed\n")
+
+
 
     def replay_with_vlc(self):
         # Replay this MP4 video file using VLC
         mp4_filepath = os.path.join(self.tempd, "streaming.mp4")
         subprocess.run(shlex.split(f"vlc --play-and-exit {mp4_filepath}"), check=True)
 
+
 def save_to_excel(df):
     # Save DataFrame to Excel file
     filename = 'gps_data4.xlsx'
     df.to_excel(filename, index=False, engine='openpyxl')
     print(f"GPS data saved to {filename}")
+
+
 
 def test_streaming():
     streaming_example = StreamingExample()
@@ -324,9 +352,10 @@ def test_streaming():
     # Perform some live video processing while the drone is flying
     streaming_example.fly()
     # Stop the video stream
-    streaming_example.stop()
+    #streaming_example.stop()
     # Recorded video stream postprocessing
     # streaming_example.replay_with_vlc()
+
 
 if __name__ == "__main__":
     test_streaming()
